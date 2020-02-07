735c0ddb61de568025ba268f5e13723e3da3dece
toirai <larsalex8@gmail.com>
2020-02-07 18:59:56 +0900

    デバイスログインテスト完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    get 'genres/index'
+  end
+  get 'topicks/top'
+  get 'topicks/index'
+  get 'topicks/show'
+  get 'topicks/edit'
+  devise_for :admins, controllers:{
+  	sessions:       "admins/sessions",
+  	passwords:      "admins/passwords",
+  	registrations:  "admins/registrations"
+  }
+  devise_for :customers, controllers:{
+  	sessions:       "customers/sessions",
+  	passwords:      "customers/passwords",
+  	registrations:  "customers/registrations"
+  }
+
+  root to: "topicks#top"
+
+  resources :topicks do
+    collection do
+      get "about"
+    end
+  end
+
+  namespace :admins do
+    resources :genres
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
