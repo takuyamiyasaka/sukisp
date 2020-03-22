54b9d85a3fef34bcc2b2b776f3a2eb119ebebccc
toirai <larsalex8@gmail.com>
2020-03-22 20:12:08 +0900

    rspec change admin_spec

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem "rspec-rails"
   gem "factory_bot_rails"
   gem "database_cleaner"
+  gem "faker"
 
 
 end
@@ -63,7 +64,6 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
-  gem "faker"
   gem "shoulda-matchers"
 
 end
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -24,6 +24,76 @@ RSpec.feature "管理者のテスト",type: :feature do
 			expect(page).to have_current_path root_path
 		end
 	end
+
+	feature "ログインいていない状態" do
+		feature "以下のページにアクセスした際のリダイレクト先" do
+			scenario "管理者のトップページへのアクセス" do
+				visit top_admins_topicks_path
+				expect(page).to have_current_path new_admin_session_path
+			end
+			scenario "管理者のジャンルページのアクセス" do
+				visit admins_genres_path
+				expect(page).to have_current_path new_admin_session_path
+			end
+			scenario "管理者の話題一覧のアクセス" do
+				visit admins_topicks_path
+				expect(page).to have_current_path new_admin_session_path
+			end
+			scenario "管理者の会員一覧のアクセス" do
+				visit admins_customers_path
+				expect(page).to have_current_path new_admin_session_path
+			end
+		end
+	end
+	feature "ログインした状態" do
+		before do
+			login(@admin)
+		end
+		feature "表示内容とリンク" do
+			scenario "ヘッダーの表記とリンク" do
+				visit top_admins_topicks_path
+				expect(page).to have_link "", href: admins_genres_path
+				expect(page).to have_content "ジャンル"
+				expect(page).to have_link "", href: admins_topicks_path
+				expect(page).to have_content "話題"
+				expect(page).to have_link "", href: admins_customers_path
+				expect(page).to have_content "会員一覧"
+				expect(page).to have_link "", href: destroy_admin_session_path
+				expect(page).to have_content "ログアウト"
+			end
+		end
+		feature "genreの作成と表示" do
+			before do
+				visit admins_genres_path
+				find_field("genre[genre_name]").set("title_a")
+			end
+			scenario "正しく保存できているか" do
+				expect {
+					find("input[name='commit']").click
+				}.to change{ Genre.count }.by(1)
+			end
+			scenario "リダイレクト先の確認" do
+				find("input[name='commit']").click
+				expect(page).to have_current_path admins_genres_path
+				expect(page).to have_content "title_a"
+			end
+		end
+		feature "genreの編集と表示" do
+			before do
+				@genre = create(:genre)
+				visit edit_admins_genre_path(@genre)
+				find_field("genre[genre_name]").set("title_b")
+				find("input[name='commit']").click
+			end
+			scenario "genreの更新" do
+				expect(page).to have_content "title_b"
+			end
+		end
+	end
 end
+# "ログインの表記とリンク"
+# "ジャンルの表記とリンク"
+# "話題の表記とリンク"
+# "会員の表記とリンク"
 
 
