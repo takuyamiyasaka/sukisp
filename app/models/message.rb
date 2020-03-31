841a09842da5819e9527c8d0aa9482eaf3029975
toirai <larsalex8@gmail.com>
2020-03-31 18:25:01 +0900

    add rspec room

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :customer
   belongs_to :room
+
+  validates :content ,presence: true
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Message, "モデルに関するテスト",type: :model do
 			is_expected.to belong_to(:room)
 		end
 	end
+
+	describe "保存のテスト" do
+		before do
+			@customer = create(:customer)
+			@room = create(:room)
+		end
+		it "正しく保存される" do
+			expect(FactoryBot.build(:message,customer_id:@customer.id,room_id:@room.id)).to be_valid
+		end
+
+		it "正しく保存されない" do
+			expect(FactoryBot.build(:message, :no_content,customer_id:@customer_id,room_id:@room.id)).to_not be_valid
+		end
+	end
 end
