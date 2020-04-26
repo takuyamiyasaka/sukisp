2e50cca4e6f7d845b7077f7e3c75181ee20fb6aa
toirai <larsalex8@gmail.com>
2020-04-26 21:25:07 +0900

    rspec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Comment, "コメントモデルのテスト", type: :model do
 			is_expected.to belong_to(:topick)
 		end
 	end
+	before do
+		@customer = create(:customer)
+			genre = create(:genre)
+			@topick = FactoryBot.create(:topick, customer_id: @customer.id,genre_id: genre.id)
+	end
+	describe "保存のテスト" do
+		it "正しく保存される" do
+			expect(FactoryBot.create(:comment, customer_id: @customer.id,topick_id: @topick.id)).to be_valid
+		end
+
+		it "正しく保存されないこと" do
+			expect(FactoryBot.build(:comment,:no_comment,customer_id: @customer.id,topick_id:@topick.id)).to_not be_valid
+		end
+	end
 end
